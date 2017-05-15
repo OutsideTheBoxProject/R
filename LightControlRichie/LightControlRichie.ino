a803b778f440cc953bb619118c03445019710552
kattaspiel <katta@igw.tuwien.ac.at>
2017-05-15 14:24:36 +0200

    general commit

--- LightControlRichie/LightControlRichie.ino
+++ LightControlRichie/LightControlRichie.ino
@@ -1,20 +1,24 @@
+
 /* ----------- includes --------------*/
 
 #include <Process.h>
 #include <TimerOne.h>
 #include <MPU9250.h>
 #include <quaternionFilters.h>
-#include <Wiegand.h>
+#include <SeeedRFIDLib.h>
+#include <SoftwareSerial.h> 
+
 
 /* ----------- defines --------------*/
 
 #define LIGHT (String)"http://192.168.0.100/api/EE2Md-XKTaIjMj9J78ztSPZruD9l9P6A0SknxsJd/lights/1/state"
-#define COLOURCOMMAND1 (String)"curl -X PUT -d '{\"hue\" : "
-#define COLOURCOMMAND2 (String)", \"sat\" : 250, \"bri\": 254, \"transitiontime\": 0 }' " + LIGHT
-#define COLOURCOMMAND2T (String)", \"sat\" : 250, \"bri\": 254, \"transitiontime\": 30 }' " + LIGHT
+#define COLOURCOMMAND1 (String)"curl -X PUT -d '{\"hue\":"
+#define COLOURCOMMAND2 (String)"}' " + LIGHT
+#define COLOURCOMMAND2T (String)"}' " + LIGHT
 #define SETBRIGHTNESS1 (String)"curl -X PUT -d '{\"bri\":"
 #define SETBRIGHTNESS2 (String)"}' " + LIGHT
 
+#define FULLON  (String)"curl -X PUT -d '{\"sat\":250, \"bri\":254}' " + LIGHT 
 #define OFF (String)"curl -X PUT -d '{\"on\":false}' " + LIGHT
 #define ON (String)"curl -X PUT -d '{\"on\":true}' " + LIGHT
 
@@ -33,20 +37,25 @@
 #define BRIGHTMAX 254
 #define BRIGHTMIN 0
 
+#define INTERVALL 4000
+
 #define AHRS false         // Set to false for basic data read
 #define SerialDebug true  // Set to true to get Serial output for debugging
 #define ADDRESS 0x71
 #define ALTADDRESS 0xFF
 
 #define MASTERCODE 10976553
+#define RFID_RX_PIN 12
+#define RFID_TX_PIN 13
 
 #define DEBUG 1
+#define TEST
 
 /* ----------- global variables --------------*/
 //lights
 const int buttonPin = 4;
 int buttonState = 0;
-bool lightON = 0;
+bool lightON = 1;
 Process p;
 int brightness = 254;
 bool brighter = 0;
@@ -56,14 +65,19 @@ const int sampleWindow = 250;
 unsigned int knock; 
 
 //MPU
-int intPin = 2;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int intPin = 5;  // These can be changed, 2 and 3 are the Arduinos ext int pins
 MPU9250 sensor;
-int sampling = 100; // measure every .1 seconds
+int sampling = 1000; // measure every .1 seconds
 int lastMillis = 0;
+bool firstRun = true;
+float previous[3] = {0.0, 0.0, 0.0};
+int prevColour = 0;
 
 //RFID
-WIEGAND wg;
-long lastRFID = 0;
+SeeedRFIDLib RFID(WIEGAND_26BIT); 
+RFIDTag tag;
+
+void play_travel();
 
 
 /* ----------- functions --------------*/
@@ -87,14 +101,6 @@ void setup() {
   {
     Serial.println("MPU9250 is online...");
 
-    // Start by performing self test and reporting values
-    sensor.MPU9250SelfTest(sensor.SelfTest);
-    Serial.print("x-axis self test: acceleration trim within : ");
-    Serial.print(sensor.SelfTest[0],1); Serial.println("% of factory value");
-
-    // Calibrate gyro and accelerometers, load biases in bias registers
-    sensor.calibrateMPU9250(sensor.gyroBias, sensor.accelBias);
-
     sensor.initMPU9250();
     // Initialize device for active mode read of acclerometer, gyroscope, and
     // temperature
@@ -106,15 +112,12 @@ void setup() {
     Serial.println(c, HEX);
     while(1) ; // Loop forever if communication doesn't happen
   }
-
-  wg.begin(5, 2, 6, 3);
-  Serial.println("RFID Reader initialised.");
   
   Serial.println("Ready to go!");
 }
 
 void loop() {
-  RFIDLoop();
+  //RFIDLoop();
   gyroLoop();
   microphoneLoop();
 
@@ -127,37 +130,37 @@ void loop() {
 }
 
 void RFIDLoop(){
-  if ( wg.available() &&  (abs(millis() - lastRFID) > 1000) ) {
-    lastRFID = millis();
-    unsigned long code = wg.getCode();
-    Serial.println(code);
-    if (code == MASTERCODE){
-      Serial.println("They connected! <3");
-    }
-  }
-  
+  if(RFID.isIdAvailable()) {
+        tag = RFID.readId();
+        // In Wiegand Mode, we only get the card code
+        Serial.print("CC = ");
+        Serial.println(tag.id); 
+   }
 }
 
 void playTravel() {
   Serial.println("Starting light show");
 
-  placeCommand(ON, 0);
-  lightON = 1;
-  placeColourCommand(RED, 1, 4000);
-  placeColourCommand(PINK, 1, 4000);
-  placeColourCommand(PURPLE, 1, 4000);
-  placeColourCommand(BLUE, 1, 4000);
-  placeColourCommand(TURQUOISE, 1, 4000);
-  placeColourCommand(DARKGREEN, 1, 4000);
-  placeColourCommand(GREEN, 1, 4000);
-  placeColourCommand(BRIGHTGREEN, 1, 4000);
-  placeColourCommand(YELLOW, 1, 4000);
-  placeColourCommand(ORANGE, 1, 4000);
-  placeColourCommand(ORANGERED, 1, 4000);
-  placeColourCommand(RED, 1, 4000);
-  placeColourCommand(PINK, 1, 4000);
-  placeColourCommand(PURPLE, 1, 4000);
-  placeColourCommand(BLUE, 1, 4000);
+  if (lightON == 0){
+    placeCommand(ON, 0);
+    lightON = 1;
+  }
+  placeCommand(FULLON, 0);
+  placeColourCommand(RED, 1, INTERVALL);
+  placeColourCommand(PINK, 1, INTERVALL);
+  placeColourCommand(PURPLE, 1, INTERVALL);
+  placeColourCommand(BLUE, 1, INTERVALL);
+  placeColourCommand(TURQUOISE, 1, INTERVALL);
+  placeColourCommand(DARKGREEN, 1, INTERVALL);
+  placeColourCommand(GREEN, 1, INTERVALL);
+  placeColourCommand(BRIGHTGREEN, 1, INTERVALL);
+  placeColourCommand(YELLOW, 1, INTERVALL);
+  placeColourCommand(ORANGE, 1, INTERVALL);
+  placeColourCommand(ORANGERED, 1, INTERVALL);
+  placeColourCommand(RED, 1, INTERVALL);
+  placeColourCommand(PINK, 1, INTERVALL);
+  placeColourCommand(PURPLE, 1, INTERVALL);
+  placeColourCommand(BLUE, 1, INTERVALL);
   if (DEBUG){
     placeCommand(OFF, 0);
     lightON = 0;
@@ -167,16 +170,16 @@ void playTravel() {
 
 void placeColourCommand(String hue, int transition, int del) {
   String command = COLOURCOMMAND1 + hue;
-  if (transition == 1) {
-    command = command + COLOURCOMMAND2T;
-  } else {
+//  if (transition == 1) {
+//    command = command + COLOURCOMMAND2T;
+//  } else {
     command = command + COLOURCOMMAND2;
-  }
+//  }
   placeCommand(command, del);
 }
 
 void placeCommand(String command, int del) {
-  Serial.println(command);
+//  Serial.println("Command is: " + command);
   p.runShellCommand(command);
   while (p.running());
   delay(del);
@@ -205,11 +208,11 @@ void microphoneLoop(){
   double volts = (peakToPeak * 3.3)/1024; 
 
   if (volts >= 1.0){
-    Serial.println("Knock Knock."); 
-    Serial.print("volts: "); 
-    Serial.println(volts);
+//    Serial.println("Knock Knock."); 
+//    Serial.print("volts: "); 
+//    Serial.println(volts);
     if (volts >= 2.1){
-      Serial.println("That's it!  That's way too loud!"); 
+//      Serial.println("That's it!  That's way too loud!"); 
       if (lightON == 0){
         placeCommand(ON, 0);   
         lightON = 1;
@@ -240,13 +243,15 @@ void microphoneLoop(){
 
 void gyroLoop()
 {
+
   if (millis() - lastMillis > sampling){
     if (sensor.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
     {  
-      sensor.readAccelData(sensor.accelCount);  // Read the x/y/z adc values
-      sensor.getAres();
-
-      sensor.ax = (float)sensor.accelCount[0]*sensor.aRes; // - accelBias[0];
+      sensor.readGyroData(sensor.gyroCount);  // Read the x/y/z adc values
+      sensor.getGres();
+      sensor.gx = (float)sensor.gyroCount[0]*sensor.gRes;
+      sensor.gy = (float)sensor.gyroCount[1]*sensor.gRes;
+      sensor.gz = (float)sensor.gyroCount[2]*sensor.gRes;
       sensor.delt_t = millis() - sensor.count;
 
       // update LCD once per half-second independent of read rate
@@ -254,11 +259,33 @@ void gyroLoop()
       {
         if(SerialDebug)
         {
-          Serial.print("ax = "); Serial.print((int)1000*sensor.ax);
-          Serial.println(" mg");
+          Serial.print("X-gyro rate: "); Serial.print(sensor.gx, 3);
+          Serial.print(" degrees/sec ");
+          Serial.print("Y-gyro rate: "); Serial.print(sensor.gy, 3);
+          //Serial.print(" degrees/sec ");
+          //Serial.print("Z-gyro rate: "); Serial.print(sensor.gz, 3);
+          Serial.println(" degrees/sec");
         }
       }
     }
     lastMillis = millis();
+    if (!firstRun){
+      int colour = abs((int)(1000*sensor.gy) - (int)previous[1]);
+      if (colour > 42700) {
+        colour = 42500;  
+      }
+      Serial.print("colour is ");
+        Serial.println(2*colour);
+      if (colour > 0){ 
+        placeColourCommand((String)(2*colour), 1, 0);
+        prevColour = colour;
+      }
+    } else {
+      firstRun = false;  
+    } 
+    previous[0] = 10*sensor.gx; 
+    previous[1] = 1000*sensor.gy;
+    previous[2] = 10*sensor.gz;
+    
   }
 }
